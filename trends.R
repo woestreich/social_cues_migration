c526b7e81cb45c62c73ac944e7163cc9a3ed99b5
Will Oestreich <woestreich@stanford.edu>
2021-09-24 15:34:32 -0700

    Updated analysis script and cleaned up .csv data file

--- trends.R
+++ trends.R
@@ -1,1 +1,23 @@
 # analyze trends in case studies from systematic review
+
+# load necessary packages
+library(tidyverse)
+rm(list = ls())
+
+# load in case study list
+cases <- read.csv('data/case_studies.csv')
+cases$year <- as.integer(cases$year)
+
+# case study count by year
+yearly_counts <- data.frame(year = 1985:2021, count = numeric(37))
+for (i in 1985:2021) {
+  yearly_counts$count[i-1984] <- sum(cases$year == i)
+  yearly_counts$year[i-1984] <- i
+}
+
+p1 <- ggplot(yearly_counts,aes(year,count)) + geom_col() +
+  theme_classic() +
+  xlab("Year") +
+  ylab("Number of case studies")
+p1
+
